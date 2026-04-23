7e137d3e7d0611bd0a0af7fd2fae7c4b786c1847
hhwkhh-sys <u1333031@o365.nuu.edu.tw>
2026-04-23 16:07:20 +0800

    123

--- databases.php
+++ databases.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $dbname = 'fullstack_app';
 $username = 'root'; // Mac MAMP 預設是 root，Windows Laragon 預設是 root
-$password = '0118Joel'; // Mac MAMP 預設是 root，Windows Laragon 預設是空字串 ''
+$password = '00000000'; // Mac MAMP 預設是 root，Windows Laragon 預設是空字串 ''
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
